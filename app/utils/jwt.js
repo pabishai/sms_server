1a7c1145b7f01b16474591871e07169a30fed91c
PAbishai <abishai.omari@andela.com>
2019-07-22 11:47:05 +0300

    trace error

--- app/utils/jwt.js
+++ app/utils/jwt.js
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken';
 
 export const generateJWTToken = (user, res) => {
-  console.log('Code reached here');
-  const payload = {
+  try {
+    const payload = {
       id: user.id,
       contact: user.contact
     };
-  try {
-    return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    console.log(payload)
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
       expiresIn: '7h',
     });
+    console.log(token)
+    return token
   } catch (error) {
     return res.status(500).send({
       message: 'Oops!. Something went wrong'
